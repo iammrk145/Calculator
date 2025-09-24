61c1b5b2ece78ab5979020eb499abb44aba337f6
Muhammad Karbhari <130045191+iammrk145@users.noreply.github.com>
2025-09-24 09:39:43 +0530

    added multiple function for calculation

---
+++ script.js
@@ -0,0 +1,30 @@
+//function for addtion
+function add(num1, num2){
+    return num1 + num2;
+}
+
+console.log(add(4,5));
+
+//function for substraction
+
+function substraction(num1, num2){
+    return num1 - num2;
+}
+
+console.log(substraction(7, 8));
+
+//function for multiply
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiply(4, 5));
+
+// function for divide
+
+function divide(num1, num2){
+    return num1 / num2
+}
+
+console.log(divide(4, 2));
