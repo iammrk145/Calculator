4804db9007df3ef6d0ff6b7baa72a6c837a37fe7
Muhammad Karbhari <130045191+iammrk145@users.noreply.github.com>
2025-09-24 10:31:52 +0530

    added a operation select function

--- script.js
+++ script.js
@@ -24,7 +24,47 @@ console.log(multiply(4, 5));
 // function for divide
 
 function divide(num1, num2){
-    return num1 / num2
+
+    if(num2 === 0){
+        return "cannot divide by 0";
+    } else {
+        return num1 / num2
+    }
+    
 }
 
 console.log(divide(4, 2));
+
+
+// function for a operation 
+
+
+function operation(operation, num1, num2){
+
+    let result;
+
+    switch(operation){
+        case "+":
+            result = add(num1, num2);
+            break;
+
+        case "-":
+            result = substraction(num1, num2);
+            break;
+
+        case "*":
+            result = multiply(num1,num2);
+            break;
+
+        case "/":
+            result = divide(num1, num2);
+            break;
+        
+        default:
+            console.log("enter a valid operation e.g '+, -, *, /'");
+    }
+
+    return result;
+}
+
+console.log(operation("-", 7, 0));
